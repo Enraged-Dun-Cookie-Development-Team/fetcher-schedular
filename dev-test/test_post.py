7055fd7a5f72e26e4ccc51eedbf4dc3dcd2aad29
YoungHector <2581131950@qq.com>
2022-12-24 21:08:11 +0800

    ✅  新增接口测试方式: 蹲饼器注册

--- dev-test/test_post.py
+++ dev-test/test_post.py
@@ -5,7 +5,8 @@ s = socket.socket()  # 创建 socket 对象
 host = socket.gethostname()  # 获取本地主机名
 port = 12345  # 设置端口
 
-url = 'http://0.0.0.0:{}'.format(port)
+# 注册蹲饼器测试
+url = 'http://0.0.0.0:{}/register'.format(port)
 input_data = {'instance_id': 'SilverAsh'}
 res = requests.post(url, json=input_data)
 print(res.content)
